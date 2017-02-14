37bb5100ec8f68bb0130b52bb2e8f4d9462bf2b5
Camillo Penna <ollimacgja@gmail.com>
2017-02-14 04:49:23 -0200

    change migrations to work

--- db/migrate/20161228173639_create_usuarios.rb
+++ db/migrate/20161228173639_create_usuarios.rb
@@ -2,8 +2,8 @@ class CreateUsuarios < ActiveRecord::Migration
   def change
     create_table :usuarios do |t|
       t.string :nome
-      t.belongs_to :user_id, index: true, foreign_key: true
-      t.belongs_to :filial_id, index: true, foreign_key: true
+      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :filial, index: true, foreign_key: true
       t.integer :tipo
 
       t.timestamps null: false
--- db/migrate/20161228180603_change_colum.rb
+++ db/migrate/20161228180603_change_colum.rb
@@ -1,6 +1,6 @@
 class ChangeColum < ActiveRecord::Migration
   def change
-    rename_column :usuarios, :user_id_id, :user_id
-    rename_column :usuarios, :filial_id_id, :filial_id
+    # rename_column :usuarios, :user_id_id, :user_id
+    # rename_column :usuarios, :filial_id_id, :filial_id
   end
 end
